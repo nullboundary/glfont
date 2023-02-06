50fecd030436702b9b5dc2b73da509d5cb775244
Mareks Ruskuls <mareks@nic.lv>
2023-02-06 16:23:57 +0200

    LoadFontBytes

--- README.md
+++ README.md
@@ -24,6 +24,14 @@ func LoadTrueTypeFont(program uint32, r io.Reader, scale int32, low, high rune, 
 ```
 LoadTrueTypeFont builds buffers and textures based on a ttf files gylphs.
 
+#### func  LoadFontFromBytes
+
+```go
+func LoadFontBytes(buf []byte, scale int32, windowWidth int, windowHeight int) (*Font, error) {
+```
+LoadFont loads font directly from bytes (such as `goregulat.TTF`, https://pkg.go.dev/golang.org/x/image/font/gofont/goregular ) at the given scale. The default character set
+is ASCII (codepoints 32 to 127).
+
 #### func (*Font) GenerateGlyphs
 
 ```go
--- font.go
+++ font.go
@@ -1,6 +1,7 @@
 package glfont
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
@@ -24,14 +25,8 @@ type color struct {
 	a float32
 }
 
-//LoadFont loads the specified font at the given scale.
-func LoadFont(file string, scale int32, windowWidth int, windowHeight int) (*Font, error) {
-	fd, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer fd.Close()
-
+// Use default preapration for exported functions like `LoadFont` and `LoadFontFromBytes`
+func configureDefaults(windowWidth int, windowHeight int) uint32 {
 	// Configure the default font vertex and fragment shaders
 	program, err := newProgram(vertexFontShader, fragmentFontShader)
 	if err != nil {
@@ -41,14 +36,35 @@ func LoadFont(file string, scale int32, windowWidth int, windowHeight int) (*Fon
 	// Activate corresponding render state
 	gl.UseProgram(program)
 
-	//set screen resolution
+	// set screen resolution
 	resUniform := gl.GetUniformLocation(program, gl.Str("resolution\x00"))
 	gl.Uniform2f(resUniform, float32(windowWidth), float32(windowHeight))
+
+	return program
+}
+
+// LoadFontBytes loads the specified font bytes at the given scale.
+func LoadFontBytes(buf []byte, scale int32, windowWidth int, windowHeight int) (*Font, error) {
+	program := configureDefaults(windowWidth, windowHeight)
+
+	fd := bytes.NewReader(buf)
+	return LoadTrueTypeFont(program, fd, scale, 32, 127, LeftToRight)
+}
+
+// LoadFont loads the specified font at the given scale.
+func LoadFont(file string, scale int32, windowWidth int, windowHeight int) (*Font, error) {
+	fd, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+
+	program := configureDefaults(windowWidth, windowHeight)
 
 	return LoadTrueTypeFont(program, fd, scale, 32, 127, LeftToRight)
 }
 
-//SetColor allows you to set the text color to be used when you draw the text
+// SetColor allows you to set the text color to be used when you draw the text
 func (f *Font) SetColor(red float32, green float32, blue float32, alpha float32) {
 	f.color.r = red
 	f.color.g = green
@@ -56,6 +72,7 @@ func (f *Font) SetColor(red float32, green float32, blue float32, alpha float32)
 	f.color.a = alpha
 }
 
+// UpdateResolution used to recalibrate fonts for new window size
 func (f *Font) UpdateResolution(windowWidth int, windowHeight int) {
 	gl.UseProgram(f.program)
 	resUniform := gl.GetUniformLocation(f.program, gl.Str("resolution\x00"))
@@ -63,7 +80,7 @@ func (f *Font) UpdateResolution(windowWidth int, windowHeight int) {
 	gl.UseProgram(0)
 }
 
-//Printf draws a string to the screen, takes a list of arguments like printf
+// Printf draws a string to the screen, takes a list of arguments like printf
 func (f *Font) Printf(x, y float32, scale float32, fs string, argv ...interface{}) error {
 
 	indices := []rune(fmt.Sprintf(fs, argv...))
@@ -72,17 +89,17 @@ func (f *Font) Printf(x, y float32, scale float32, fs string, argv ...interface{
 		return nil
 	}
 
-	//setup blending mode
+	// setup blending mode
 	gl.Enable(gl.BLEND)
 	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
 
 	// Activate corresponding render state
 	gl.UseProgram(f.program)
-	//set text color
+	// set text color
 	gl.Uniform4f(gl.GetUniformLocation(f.program, gl.Str("textColor\x00")), f.color.r, f.color.g, f.color.b, f.color.a)
-	//set screen resolution
-	//resUniform := gl.GetUniformLocation(f.program, gl.Str("resolution\x00"))
-	//gl.Uniform2f(resUniform, float32(2560), float32(1440))
+	// set screen resolution
+	// resUniform := gl.GetUniformLocation(f.program, gl.Str("resolution\x00"))
+	// gl.Uniform2f(resUniform, float32(2560), float32(1440))
 
 	gl.ActiveTexture(gl.TEXTURE0)
 	gl.BindVertexArray(f.vao)
@@ -90,26 +107,26 @@ func (f *Font) Printf(x, y float32, scale float32, fs string, argv ...interface{
 	// Iterate through all characters in string
 	for i := range indices {
 
-		//get rune
+		// get rune
 		runeIndex := indices[i]
 
-		//find rune in fontChar list
+		// find rune in fontChar list
 		ch, ok := f.fontChar[runeIndex]
 
-		//load missing runes in batches of 32
+		// load missing runes in batches of 32
 		if !ok {
 			low := runeIndex - (runeIndex % 32)
-			f.GenerateGlyphs(low, low + 31)
+			f.GenerateGlyphs(low, low+31)
 			ch, ok = f.fontChar[runeIndex]
 		}
 
-		//skip runes that are not in font chacter range
+		// skip runes that are not in font chacter range
 		if !ok {
 			fmt.Printf("%c %d\n", runeIndex, runeIndex)
 			continue
 		}
 
-		//calculate position and size for current rune
+		// calculate position and size for current rune
 		xpos := x + float32(ch.bearingH)*scale
 		ypos := y - float32(ch.height-ch.bearingV)*scale
 		w := float32(ch.width) * scale
@@ -129,7 +146,7 @@ func (f *Font) Printf(x, y float32, scale float32, fs string, argv ...interface{
 		// Update content of VBO memory
 		gl.BindBuffer(gl.ARRAY_BUFFER, f.vbo)
 
-		//BufferSubData(target Enum, offset int, data []byte)
+		// BufferSubData(target Enum, offset int, data []byte)
 		gl.BufferSubData(gl.ARRAY_BUFFER, 0, len(vertices)*4, gl.Ptr(vertices)) // Be sure to use glBufferSubData and not glBufferData
 		// Render quad
 		gl.DrawArrays(gl.TRIANGLES, 0, 16)
@@ -140,7 +157,7 @@ func (f *Font) Printf(x, y float32, scale float32, fs string, argv ...interface{
 
 	}
 
-	//clear opengl textures and programs
+	// clear opengl textures and programs
 	gl.BindVertexArray(0)
 	gl.BindTexture(gl.TEXTURE_2D, 0)
 	gl.UseProgram(0)
@@ -149,7 +166,7 @@ func (f *Font) Printf(x, y float32, scale float32, fs string, argv ...interface{
 	return nil
 }
 
-//Width returns the width of a piece of text in pixels
+// Width returns the width of a piece of text in pixels
 func (f *Font) Width(scale float32, fs string, argv ...interface{}) float32 {
 
 	var width float32
@@ -163,20 +180,20 @@ func (f *Font) Width(scale float32, fs string, argv ...interface{}) float32 {
 	// Iterate through all characters in string
 	for i := range indices {
 
-		//get rune
+		// get rune
 		runeIndex := indices[i]
 
-		//find rune in fontChar list
+		// find rune in fontChar list
 		ch, ok := f.fontChar[runeIndex]
 
-		//load missing runes in batches of 32
+		// load missing runes in batches of 32
 		if !ok {
-			low := runeIndex & rune(32 - 1)
-			f.GenerateGlyphs(low, low + 31)
+			low := runeIndex & rune(32-1)
+			f.GenerateGlyphs(low, low+31)
 			ch, ok = f.fontChar[runeIndex]
 		}
 
-		//skip runes that are not in font chacter range
+		// skip runes that are not in font chacter range
 		if !ok {
 			fmt.Printf("%c %d\n", runeIndex, runeIndex)
 			continue
